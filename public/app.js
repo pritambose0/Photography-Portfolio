79b128df9121779b0a7276a7e3c2b338b1562e91
Pritam <pritambose040@gmail.com>
2024-03-10 11:31:45 +0530

    header optimise

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@
 
 window.addEventListener("scroll", function() {
     var header = document.getElementById("navbar");
-    if (window.scrollY > 200) {
+    if (window.scrollY > 100) {
       header.classList.add("bg-dark");
       header.classList.add("shadow-lg");
       header.classList.remove("bg-transparent");
